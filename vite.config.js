f991129cbec7e4736b654d019943f30142f85eac
Klinsmann Ouma <oumamalaki@gmail.com>
2025-10-07 17:22:22 +0300

    updating to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ouma-portfolio',
+  base: '/',
 
   server: {
     port: 3000},
